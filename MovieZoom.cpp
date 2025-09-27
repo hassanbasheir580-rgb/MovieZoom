e6351e3fad7accdea0029653b30e517a251b5299
Hassan Abdelhaleem Hassan Basheir <hassanbasheir580@gmail.com>
2025-09-28 03:21:22 +0800

    Add files via upload

--- MovieZoom.cpp
+++ MovieZoom.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <vector>
 #include <limits>
- 
+
 using namespace std;
 
 // A struct to hold each title's name and its short description
@@ -156,5 +156,4 @@ int main()
 
     cout << "Thanks for using MovieZoom. Goodbye!\n";
     return 0;
-
 }
